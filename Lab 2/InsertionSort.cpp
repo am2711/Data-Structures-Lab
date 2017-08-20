2b02645aac03af34a6ee6e1a252bdadbf66b01e4
Ayush Malviya <ayushmalviya27@gmail.com>
2017-08-20 17:03:18 +0530

    Lab 4 added

--- Lab 2/InsertionSort.cpp
+++ Lab 2/InsertionSort.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
-#include <conio.h>
+#include <cstdio>
 
-void insort(int a[],int n)
-{
- for(int i=0;i<n;i++)
- {
-    int j=i+1;
- }
 
-}
 
-int main()
-{
-int a[100],n;
-clrscr();
-cout<<"Enter size of array: ";
-cin>>n;
-cout<<"Enter array elements: ";
-for(i=0;i<n;i++)
-	cin>>a[i];
-cout<<"The sorted array is: ";
-insort(a,n);
-for(i=0;i<n;i++)
-	cout<<a[i]<<" ";
-getch();
+int main() {
+	
+	int n;
+	while(n!=42)
+	{
+	scanf("%d",&n);
+	if(n==42)
+		break;
+	printf("%d\n", n);
+		
+	}// your code here
+
 return 0;
 }
